055198dfc3186869ab01221a42d98f96ac070554
Jens Kursch <106172039+jens-kursch-NHSD@users.noreply.github.com>
2023-11-21 13:44:53 +0000

    Use overwrite_msteams_webhook_cloudwatch_alarm

--- ssm.tf
+++ ssm.tf
@@ -11,5 +11,5 @@ resource "aws_ssm_parameter" "cloudwatch_webhook" {
   type      = "SecureString"
   value     = var.msteams_webhook_cloudwatch_alarm
   key_id    = "alias/aws/ssm"
-  overwrite = var.msteams_webhook_cloudwatch_alarm
+  overwrite = var.overwrite_msteams_webhook_cloudwatch_alarm
 }
