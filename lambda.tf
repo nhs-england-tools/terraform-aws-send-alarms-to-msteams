7ba7bb4243ab951c8ded7a72752c09938ecf07be
Will Smith <224611313+william-smith-NHSD@users.noreply.github.com>
2026-05-14 14:04:09 +0100

    remove deprecated python versions

--- lambda.tf
+++ lambda.tf
@@ -25,7 +25,7 @@ resource "aws_lambda_layer_version" "python_dependencies_layer" {
   layer_name          = "${var.prefix}-msteams-layer"
   filename            = data.archive_file.layer.output_path
   source_code_hash    = data.archive_file.layer.output_base64sha256
-  compatible_runtimes = ["python3.14", "python3.9", "python3.8", "python3.7"]
+  compatible_runtimes = ["python3.14", "python3.9"]
 }
 
 resource "aws_lambda_function" "lambda_function" {
